5f525ab0d667bf98fdc3f16533fa61c26374f67f
Alex <naplle58@gmail.com>
2024-03-02 22:16:35 +0300

    started migration to ratatui

---
+++ src/dictionary/mod.rs
@@ -0,0 +1,44 @@
+use rand::seq::SliceRandom;
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+pub struct Dictionary {
+    words: Vec<String>,
+}
+
+pub fn new() -> Dictionary {
+    Dictionary { words: vec![] }
+}
+
+impl Dictionary {
+    /// load_dict_file accepts path for a text file, where words are
+    /// separated by a newline (\n) char.
+    pub fn load_dict_file(&mut self, path: &str) -> io::Result<()> {
+        let file = File::open(path)?;
+
+        let words = BufReader::new(file)
+            .lines()
+            .map(|e| match e {
+                Ok(line) => line,
+                Err(_) => todo!(),
+            })
+            .collect::<Vec<String>>();
+
+        self.words = words;
+
+        Ok(())
+    }
+
+    pub fn random_words(&self, n: u32) -> Vec<String> {
+        self.words
+            .choose_multiple(&mut rand::thread_rng(), n as usize)
+            .map(|s| s.to_owned())
+            .collect()
+    }
+
+    pub fn random_word(self) -> String {
+        self.random_words(1).get(0).expect("random word").to_owned()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,10 @@
-mod dict;
-mod term;
+mod dictionary;
+mod typer;
 
-fn main() -> ! {
-    let mut all_words = dict::random_words(2).join(" ");
+fn main() {
+    let mut dict = dictionary::new();
+    dict.load_dict_file("./dict/en.txt").expect("dict loaded");
 
-    let mut term = term::new().expect("valid term");
-    let mut written = String::new();
-    let mut current_pos = 0;
-    let mut miss_char: Option<char> = None;
-
-    loop {
-        term.clear();
-        term.write_target(&all_words);
-        term.write_user_input(written.as_str());
-        if let Some(pc) = miss_char {
-            term.write_user_miss(format!("miss: {}", pc).as_str());
-            miss_char = None;
-        }
-        // term.move_cursor_to(written.len(), 1);
-
-        let input_char = term.get_input_char();
-
-        let pos_char = all_words.chars().nth(current_pos);
-        if let Some(cur_target_char) = pos_char {
-            if input_char == cur_target_char {
-                current_pos += 1;
-                written.push(input_char);
-
-                if written.len() == all_words.len() {
-                    all_words = dict::random_words(1).join(" ");
-                    current_pos = 0;
-                    written.clear();
-                }
-            } else {
-                miss_char = Some(input_char);
-            }
-        }
-    }
+    let mut term = typer::new(dict);
+    term.run()
 }
